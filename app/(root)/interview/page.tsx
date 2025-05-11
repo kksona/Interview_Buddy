d46d701502ac0479811e42d596ef73509703952b
Kshitij Kumar <kksona2004@gmail.com>
2025-05-11 19:32:48 +0530

    implemeted the voice agent

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,14 @@
 import Agent from '@/components/Agent'
+import { getCurrentUser } from '@/lib/actions/auth.actions'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+
   return (
     <>
     <h3>Interview Generation</h3>
-    <Agent userName="Me" userId="user0" type="generate"/>
+    <Agent userName={user?.name!} userId={user?.id} type="generate"/>
     </>
   )
 }
