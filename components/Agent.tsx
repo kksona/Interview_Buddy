9a322a37725dd4a591a4a80be81e722f9dcba6ae
Kshitij Kumar <kksona2004@gmail.com>
2025-05-14 10:34:18 +0530

    implemented feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
+
 import { cn } from '@/lib/utils';
 import Image from 'next/image'
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import {vapi} from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -59,10 +61,11 @@ const Agent = ({userName, userId, type, interviewId, questions}:AgentProps) => {
     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
         console.log("generate feedback here");
 
-        const {success, id} = {
-            success: true,
-            id:"feedback-id",
-        }
+        const {success, feedbackId: id} = await createFeedback({
+            userId: userId!,
+            interviewId: interviewId!,
+            transcript: messages,
+        })
 
         if(success&&id){
             router.push(`/interview/${interviewId}/feedback`);
